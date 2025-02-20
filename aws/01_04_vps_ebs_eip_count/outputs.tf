91b6189ccd710e6797544a190e100fa07ec4e5a3
Eduardo de Lamo Tellez <edelamot@emeal.nttdata.com>
2025-02-20 11:29:11 +0100

    Configuración del código Terraform.

--- aws/01_04_vps_ebs_eip_count/outputs.tf
+++ aws/01_04_vps_ebs_eip_count/outputs.tf
@@ -1,23 +1,11 @@
-# output "instance_ip" {
-#   description = "The public ip for the instance"
-#   value = aws_instance.web
-# }
 output "instance_public_ips" {
-  description = "Las IPs públicas de las instancias web"
+  description = "Las IPs públicas de las instancias web: "
   value       = aws_instance.web[*].public_ip
 }
 
 output "ssh_commands" {
-  description = "Comandos SSH para las instancias web"
+  description = "Comandos SSH para las instancias web: "
   value = [
     for ip in aws_instance.web[*].public_ip : "ssh -l ubuntu ${ip}"
   ]
 }
-
-
-# output "ssh" {
-#   value = "ssh -l ubuntu ${aws_instance.web.*.public_ip}"
-# }
-# output "url" {
-#   value = "https://${aws_instance.web.*.public_ip}/"
-# }
--- aws/01_04_vps_ebs_eip_count/vars.tf
+++ aws/01_04_vps_ebs_eip_count/vars.tf
@@ -2,26 +2,26 @@ variable "ssh_key_path" {
   type = string
 }
 variable "project_name" {
-  type = string
-  default = "terraform"
+  type    = string
+  default = "terraform-usuario03"
 }
 variable "region_name" {
-  type = string
+  type    = string
   default = "eu-west-3"
 }
 variable "availability_zone" {
-  type = string
+  type    = string
   default = "eu-west-3a"
 }
-variable "vpc_id"{
+variable "vpc_id" {
   type = string
 }
 variable "instance_type" {
-  type = string
+  type    = string
   default = "t3.micro"
 }
 
 variable "count_value" {
-  type = number
+  type    = number
   default = 2
 }

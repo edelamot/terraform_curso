6fe4449cc214342d59e7c4fb4bd607424c94605c
Eduardo de Lamo Tellez <edelamot@emeal.nttdata.com>
2025-02-21 10:38:10 +0100

    terraform fmt

--- aws/09_s3_bucket/main.tf
+++ aws/09_s3_bucket/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region      = var.region
+  region = var.region
 }
 
 resource "aws_s3_bucket" "b" {
--- aws/09_s3_bucket/vars.tf
+++ aws/09_s3_bucket/vars.tf
@@ -1,24 +1,24 @@
 variable "region" {
-    type = string
-    default = "eu-west-3"
+  type    = string
+  default = "eu-west-3"
 }
 
 variable "bucket_name" {
-  type = string
+  type    = string
   default = "My bucket"
 }
 
 variable "acl_value" {
-  type = string
+  type    = string
   default = "private"
 }
 
 variable "project_name" {
-  type = string
+  type    = string
   default = "terraform-usuario03"
 }
 
 variable "client_name" {
-  type = string
+  type    = string
   default = "Eduardo"
 }
